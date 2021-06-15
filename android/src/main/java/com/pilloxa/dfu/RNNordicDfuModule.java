18628b532db5b231fce3ffc09e67b72825a7debb
Bogdan Preda <preda.bogdan86@yahoo.com>
2021-06-15 13:30:14 +0300

    Update RNNordicDfuModule.java

--- android/src/main/java/com/pilloxa/dfu/RNNordicDfuModule.java
+++ android/src/main/java/com/pilloxa/dfu/RNNordicDfuModule.java
@@ -36,9 +36,6 @@ public class RNNordicDfuModule extends ReactContextBaseJavaModule implements Lif
         final DfuServiceInitiator starter = new DfuServiceInitiator(address)
                 .setKeepBond(false);
         
-        // set PRN = 1
-        // starter.setPacketsReceiptNotificationsValue(1);
-
         if (options.hasKey("retries")) {
           int retries = options.getInt("retries");
           starter.setNumberOfRetries(retries);
